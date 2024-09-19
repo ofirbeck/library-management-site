220123076955b93a3ef8d912f759ee8c51bea542
Ofir Beck <ofirbeck987@gmail.com>
2024-09-19 17:15:05 +0300

    added endpoint usage in the client, fixed CORS error

--- client/app/src/App.jsx
+++ client/app/src/App.jsx
@@ -1,18 +1,60 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
+  const [books, setBooks] = useState([]);
+  const [title, setTitle] = useState("");
+  const [releaseYear, setReleaseYear] = useState("");
+  const getBooks = async () => {
+    try {
+      const response = await fetch("http://127.0.0.1:8000/api/books/");
+      const data = await response.json();
+      setBooks(data);
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  const createNewBook = async () => {
+      const bookData = {
+        title: title,
+        year_of_release: releaseYear
+      };
+      try {
+        const response = await fetch("http://127.0.0.1:8000/api/books/create/", {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify(bookData),
+        });
+        const data = await response.json();
+        setBooks([...books, data]);
+    }
+    catch (error) {
+      console.log(error);
+    }
+  }
+  useEffect(() => {
+    getBooks();
+  }, []);
+
   return (
     <>
       <h1>Book archive</h1>
       <div>
-        <input type="text" placeholder="Book title" />
-        <input type="number" placeholder="Release Date"/>
-        <button>Add Book to the archive</button>
-
+        <input type="text" placeholder="Book title" onChange={(e) => setTitle(e.target.value)}/>
+        <input type="number" placeholder="Release Date" onChange={(e) => setReleaseYear(e.target.value)}/>
+        <button onClick={createNewBook}>Add Book to the archive</button>
       </div>
+      {books.map((book) => (
+        <div>
+          <h4>Title: {book.title}</h4>
+          <p>Year of release: {book.year_of_release}</p>
+        </div>
+      ))}
     </>
   )
 }
--- server/project/project/settings.py
+++ server/project/project/settings.py
@@ -37,8 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'api',
     'rest_framework',
-    'api'
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
@@ -49,6 +50,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
 ]
 
 ROOT_URLCONF = 'project.urls'
@@ -123,3 +125,6 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# allowing all websites to access my api, should change this in production
+CORS_ALLOW_ALL_ORIGINS = True
